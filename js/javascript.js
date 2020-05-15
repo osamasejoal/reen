820ae3d2bafb6f792c53591b1b7dce411377392f
osamashaheb <65346178+osamashaheb@users.noreply.github.com>
2020-05-15 18:15:07 +0600

    reen psd file

---
+++ js/javascript.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {   
+
+
+/* Hamburger Menu */
+
+$(".menu").click(function() {
+	$(".menu").toggleClass("active");
+	$(".navbar-menu").toggleClass("active");
+});
+
+/* End */
+
+   
+})
+
+
+
